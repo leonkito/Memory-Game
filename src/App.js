a2a0844cd6b7f8c4910cfa9c588a3940a2726c4a
Leonardo Niekiforuk <leonardoniekiforuk@hotmail.com>
2022-10-06 17:11:41 -0300

    Add functional score and best score

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-
 import React, {useState} from "react";
+import Deck from './components/deck';
+
 const App = () => {
   const[message] = useState("Pests Memory Game")
   const[score,setScore] = useState(0)
   const[bestScore,setBestScore] = useState(0)
+
+  const handleClick = () => {
+    setScore(score+1);
+  };
+  
+  const defineBest =()=>{
+    if(score>bestScore){
+      setBestScore(score);
+    }
+    setScore(0);
+  }
+
   return (
-    <div className="header">
-      <div className="title">{message}</div>
-      <div className="score-holder">
-        <div>Score: {score}</div>
-        <div>Best Score: {bestScore}</div>
+    <>
+      <div className="header">
+        <div className="title">{message}</div>
+        <div className="score-holder">
+          <div>Score: {score}</div>
+          <div>Best Score: {bestScore}</div>
+        </div>
       </div>
-    </div>
+      <Deck defineBest={defineBest} onClick={handleClick}/>
+    </>
   )
 }
 
